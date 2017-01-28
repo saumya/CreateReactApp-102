62d9436b0602dab0252565628041e2b246f2fbdc
saumya <2saumyaray@gmail.com>
2017-01-28 10:52:48 +0530

    playing with components

--- src/comps/AppHome.container.js
+++ src/comps/AppHome.container.js
@@ -10,18 +10,16 @@ class AppHome extends Component {
         const contentString = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.'
         return (
             <div>
-                <ContentWithHeader headerText='Welcome' contentText={contentString} />
-                <ContentWithHeader headerText='Sub Heading' contentText={contentString} />
-
-                <Container>
-
+                <ContentWithHeader headerText='Site Title' contentText={contentString} />
                 <LoginRegisterView />
+
+                <ContentWithHeader headerText='Introduction' contentText={contentString} />
                 <Segment>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...
                 <Divider section />
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...
                 </Segment>
-                </Container>
+                
 
                 <ContentWithHeader headerText='Footer Notes' contentText={contentString} />
             </div>
