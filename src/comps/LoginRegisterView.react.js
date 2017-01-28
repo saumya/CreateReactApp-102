6ab01e4e65512fe9bb7aa8f3edac20ff88e186a4
saumya <2saumyaray@gmail.com>
2017-01-28 11:28:58 +0530

    playing with components

--- src/comps/LoginRegisterView.react.js
+++ src/comps/LoginRegisterView.react.js
@@ -1,6 +1,6 @@
 //
 import React, {Component} from 'react'
-import { Container, Grid, Header, Segment, Button, Divider } from 'semantic-ui-react'
+import { Container, Grid, Header, Segment, Button, Divider, Input } from 'semantic-ui-react'
 
 class LoginRegisterView extends Component {
     render(){
@@ -11,13 +11,24 @@ class LoginRegisterView extends Component {
                 <Grid columns={2} relaxed>
                     <Grid.Column>
                         <Segment padded>
-                        <Button primary>Login</Button>
+                            <Input fluid placeholder='User Name' />
+                                <Divider hidden />
+                            <Input fluid placeholder='Password' />
+                                <Divider hidden />
+                            <Button primary fluid>Login</Button>
                         </Segment>
                     </Grid.Column>
 
                     <Grid.Column>
                         <Segment basic>
-                        <Button secondary>Sign Up Now</Button>
+                            <Container fluid>
+                                <Header as='h3'>Not yet a menber?</Header>
+                                <p>
+                                    There are benifits of being a member. All that is needed is a Facebook or Google account. Well, if you wish, create an account with us.
+                                </p>
+                            </Container>
+                            <Divider hidden />
+                            <Button secondary>Sign Up Now</Button>
                         </Segment>
                     </Grid.Column>
                 </Grid>
