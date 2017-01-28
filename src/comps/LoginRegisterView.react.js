01039df6e7d6eaaa3ccc2207da5ed1d9e46886e0
saumya <2saumyaray@gmail.com>
2017-01-28 10:47:39 +0530

    playing with components

--- src/comps/LoginRegisterView.react.js
+++ src/comps/LoginRegisterView.react.js
@@ -6,13 +6,21 @@ class LoginRegisterView extends Component {
     render(){
         return(
             <Container>
-                <Header as='h1'>Login / Register</Header>
-                <Segment padded>
-                    <Button primary>Login</Button>
-                    <Divider vertical>Or</Divider>
-                    <Button secondary>Sign Up Now</Button>
-                </Segment>
+                <Header as='h1'>Welcome</Header>
                 
+                <Grid columns={2} relaxed>
+                    <Grid.Column>
+                        <Segment padded>
+                        <Button primary>Login</Button>
+                        </Segment>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                        <Segment basic>
+                        <Button secondary>Sign Up Now</Button>
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
                
             </Container>
         )
